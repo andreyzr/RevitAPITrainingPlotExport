b056361e1b8fa8bb8c1374ad5b414b516bfd1708
andreyzr43 <andreyzr43@yandex.ru>
2023-08-30 10:21:03 +0300

    1

--- RevitAPITrainingPlotExport/Main.cs
+++ RevitAPITrainingPlotExport/Main.cs
@@ -16,7 +16,28 @@ namespace RevitAPITrainingPlotExport
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             Document doc = commandData.Application.ActiveUIDocument.Document;
-            var sheets=new FilteredElementCollector(doc)
+
+            using(var ts =new Transaction(doc,"export dwg"))
+            {
+                ts.Start();
+                ViewPlan viewPlan = new FilteredElementCollector(doc)
+                                    .OfClass(typeof(ViewPlan))
+                                    .Cast<ViewPlan>()
+                                    .FirstOrDefault( v => v.ViewType == ViewType.FloorPlan &&
+                                    v.Name.Equals("Level 1"));
+                var dwgOption = new DWGExportOptions();
+                doc.Export(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "export.dwg",
+                    new List<ElementId> { viewPlan.Id },dwgOption);
+                ts.Commit();
+            }
+
+           
+            return Result.Succeeded;
+        }
+
+        public void BatchPrint(Document doc)
+        {
+            var sheets = new FilteredElementCollector(doc)
                             .WhereElementIsNotElementType()
                             .OfClass(typeof(ViewSheet))
                             .Cast<ViewSheet>()
@@ -27,20 +48,17 @@ namespace RevitAPITrainingPlotExport
             var viweSetes = new List<ViewSet>();
 
             PrintManager printManager = doc.PrintManager;
-
-
+            printManager.SelectNewPrintDriver("PDF24");
             printManager.PrintRange = PrintRange.Select;
-
-
-            ViewSheetSetting viewSheetSetting=printManager.ViewSheetSetting;
+            ViewSheetSetting viewSheetSetting = printManager.ViewSheetSetting;
 
             foreach (var groupedSheet in groupedSheets)
             {
-                if(groupedSheet.Key==null) continue;
+                if (groupedSheet.Key == null) continue;//
 
-                var viewSet=new ViewSet();
+                var viewSet = new ViewSet();
 
-                var sheetsOfGroup=groupedSheet.Select(s=>s).ToList();
+                var sheetsOfGroup = groupedSheet.Select(s => s).ToList();
                 foreach (var sheet in sheetsOfGroup)
                 {
                     viewSet.Insert(sheet);
@@ -51,16 +69,40 @@ namespace RevitAPITrainingPlotExport
                 printManager.PrintRange = PrintRange.Select;
                 viewSheetSetting.CurrentViewSheetSet.Views = viewSet;
 
-                using(var ts=new Transaction(doc,"Create view set"))
+                using (var ts = new Transaction(doc, "Create view set"))
                 {
                     ts.Start();
-                    viewSheetSetting.SaveAs($"{groupedSheet.Key}_{Guid.NewGuid()}");
+                    viewSheetSetting.SaveAs($"{groupedSheet.Key}_{Guid.NewGuid()}");//Создание набора листов
                     ts.Commit();
                 }
+
+
+                bool isFormatSelected = false;
+                foreach (PaperSize paperSize in printManager.PaperSizes)//Вывод на печать
+                {
+
+                    if (string.Equals(groupedSheet.Key, "А4К") &&
+                        string.Equals(paperSize.Name, "A3"))
+                    {
+                        printManager.PrintSetup.CurrentPrintSetting.PrintParameters.PaperSize = paperSize;
+                        printManager.PrintSetup.CurrentPrintSetting.PrintParameters.PageOrientation = PageOrientationType.Portrait;
+                        isFormatSelected = true;
+                    }
+                    else if (string.Equals(groupedSheet.Key, "А3А") &&
+                        string.Equals(paperSize.Name, "A3"))
+                    {
+                        printManager.PrintSetup.CurrentPrintSetting.PrintParameters.PaperSize = paperSize;
+                        printManager.PrintSetup.CurrentPrintSetting.PrintParameters.PageOrientation = PageOrientationType.Landscape;
+                        isFormatSelected = true;
+                    }
+                }
+                if (!isFormatSelected)
+                {
+                    TaskDialog.Show("Ошибка", "Не найден формат");
+                }
+                printManager.CombinedFile = false;// сохранение в разные файлы
+                printManager.SubmitPrint();//Печатаем с текущими настройками
             }
-
-
-            return Result.Succeeded;
-        }
+        }   //Печать и и созданеи наборов 
     }
 }
